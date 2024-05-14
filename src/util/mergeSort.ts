133fe9b14148250cdf4039fc90a221763f6a2abd
Dean Kadri <deankadri@Minas-MacBook-Air.local>
2024-05-14 15:11:56 -0600

    commented out unused

--- src/util/mergeSort.ts
+++ src/util/mergeSort.ts
@@ -8,23 +8,23 @@ const mergeSort: AlgorithmFnType = (init) => {
         return { steps: [arr], key };
     }
     const mid = Math.floor(arr.length / 2);
-    const leftArr = arr.slice(0, mid);
-    const rightArr = arr.slice(mid);
+    // const leftArr = arr.slice(0, mid);
+    // const rightArr = arr.slice(mid);
     // return merge(mergeSort(leftArr), mergeSort(rightArr));
     return { steps, key }
 }
 
 
-const merge = (leftArr: number[], rightArr: number[]) => {
-    const sortedArr = [];
-    while (leftArr.length && rightArr.length) {
-        if (leftArr[0] <= rightArr[0]) {
-            sortedArr.push(leftArr.shift())
-        } else {
-            sortedArr.push(rightArr.shift())
-        }
-    }
-    return [...sortedArr, ...leftArr, ...rightArr]
-}
+// const merge = (leftArr: number[], rightArr: number[]) => {
+//     const sortedArr = [];
+//     while (leftArr.length && rightArr.length) {
+//         if (leftArr[0] <= rightArr[0]) {
+//             sortedArr.push(leftArr.shift())
+//         } else {
+//             sortedArr.push(rightArr.shift())
+//         }
+//     }
+//     return [...sortedArr, ...leftArr, ...rightArr]
+// }
 
 export default mergeSort
